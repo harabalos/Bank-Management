ff59128e2cbeacd8f1807ceb1ce730b87948b26f
harabalos <babismetaxas000@gmail.com>
2022-10-18 21:54:09 +0300

    Implemented the Taxable Interface

--- src/main/model/account/Chequing.java
+++ src/main/model/account/Chequing.java
@@ -1,9 +1,13 @@
 package src.main.model.account;
 
-public class Chequing extends Account {
+import src.main.model.account.impl.Taxable;
+
+public class Chequing extends Account implements Taxable {
 
     private static final double OVERDRAFT_FEE = 5.50;
-    private static final int OVERDRAFT_LIMIT = 200;
+    private static final double OVERDRAFT_LIMIT = 200;
+    private static final double TAXABLE_INCOME = 3000;
+    private static final double TAX_RATE = 0.15;
 
     public Chequing(String id, String name, double balance) {
         super(id, name, balance);
@@ -30,6 +34,12 @@ public class Chequing extends Account {
         }
         return true;
 
+    }
+
+    @Override
+    public void tax(double income) {
+        double tax = Math.max(0, income - TAXABLE_INCOME) * TAX_RATE;
+        super.setBalance(super.round(super.getBalance() - tax));
     }
 
 }
