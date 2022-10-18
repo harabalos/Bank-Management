7b2cec76cd93202836aca508e3cdd7227440ad73
harabalos <babismetaxas000@gmail.com>
2022-10-19 01:07:12 +0300

    FINISHED THE MAIN(P2). PROGRAM IS READY

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,16 +3,18 @@ package src.main;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 import src.main.model.Bank;
+import src.main.model.Transaction;
 import src.main.model.account.Account;
 
 public class Main {
 
     static String ACCOUNTS_FILE = "src/main/data/accounts.txt";
-    static String TRANSACTIONS_FILE = "src/main/data/tramsactions.txt";
+    static String TRANSACTIONS_FILE = "src/main/data/transactions.txt";
 
     static Bank bank = new Bank();
 
@@ -20,10 +22,20 @@ public class Main {
 
         try {
             ArrayList<Account> accounts = returnAccounts();
-            System.out.println();
+            loadAccounts(accounts);
+
+            ArrayList<Transaction> transactions = returnTransactions();
+            runTransactions(transactions);
+            bank.deductTaxes();
+            for (Account account : accounts) {
+                System.out.println("\n\t\t\t\t\t ACCOUNT\n\n\t" + account + "\n\n");
+                transactionHistory(account.getId());
+            }
+
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
+
     }
 
     public static Account createObject(String[] values) {
@@ -31,12 +43,10 @@ public class Main {
             return (Account) Class.forName("src.main.model.account." + values[0])
                     .getConstructor(String.class, String.class, double.class)
                     .newInstance(values[1], values[2], Double.parseDouble(values[3]));
-
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
         }
-
     }
 
     public static void loadAccounts(ArrayList<Account> accounts) {
@@ -56,6 +66,38 @@ public class Main {
         }
         scan.close();
         return accounts;
+    }
+
+    public static ArrayList<Transaction> returnTransactions() throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream(TRANSACTIONS_FILE);
+        Scanner scan = new Scanner(fis);
+
+        ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+        while (scan.hasNextLine()) {
+            String[] values = scan.nextLine().split(",");
+            transactions.add(new Transaction(Transaction.Type.valueOf(values[1]), Long.parseLong(values[0]), values[2],
+                    Double.parseDouble(values[3])));
+        }
+
+        scan.close();
+        Collections.sort(transactions);
+        return transactions;
+    }
+
+    public static void runTransactions(ArrayList<Transaction> transactions) {
+        for (Transaction transaction : transactions) {
+            bank.executeTransaction(transaction);
+        }
+    }
+
+    public static void transactionHistory(String id) {
+        System.out.println("\t\t\t\t   TRANSACTION HISTORY\n\t");
+        for (Transaction transaction : bank.getTransactions(id)) {
+            wait(300);
+            System.out.println("\t" + transaction + "\n");
+        }
+        System.out.println("\n\t\t\t\t\tAFTER TAX\n");
+        System.out.println("\t" + bank.getAccount(id) + "\n\n\n\n");
     }
 
     public static void wait(int milliseconds) {
