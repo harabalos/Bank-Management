87944086fc31d6c540c5098ef81748e751665409
harabalos <babismetaxas000@gmail.com>
2022-10-19 00:11:45 +0300

    Finalized the Bank class.Part2

--- src/main/model/Bank.java
+++ src/main/model/Bank.java
@@ -1,10 +1,13 @@
 package src.main.model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import src.main.model.account.Account;
+import src.main.model.account.Chequing;
+import src.main.model.account.impl.Taxable;
 
 public class Bank {
 
@@ -20,6 +23,7 @@ public class Bank {
         List<Transaction> list = this.transactions.stream()
                 .filter((transaction) -> transaction.getId().equals(accountId))
                 .collect(Collectors.toList());
+
         return list.toArray(new Transaction[list.size()]);
     }
 
@@ -34,8 +38,54 @@ public class Bank {
         this.accounts.add(account.clone());
     }
 
-    public void addTransaction(Transaction transaction) {
+    private void addTransaction(Transaction transaction) {
         this.transactions.add(new Transaction(transaction));
+    }
+
+    public void executeTransaction(Transaction transaction) {
+        switch (transaction.getType()) {
+            case WITHDRAW:
+                withdrawTransaction(transaction);
+                break;
+            case DEPOSIT:
+                depositTransaction(transaction);
+                break;
+        }
+    }
+
+    private void withdrawTransaction(Transaction transaction) {
+        if (getAccount(transaction.getId()).withdraw(transaction.getAmount())) {
+            addTransaction(transaction);
+        }
+    }
+
+    private void depositTransaction(Transaction transaction) {
+        getAccount(transaction.getId()).deposit(transaction.getAmount());
+        addTransaction(transaction);
+    }
+
+    private double getIncome(Taxable account) {
+        Transaction[] transactions = getTransactions(((Chequing) account).getId());
+        return Arrays.stream(transactions)
+                .mapToDouble((transaction) -> {
+                    switch (transaction.getType()) {
+                        case WITHDRAW:
+                            return -transaction.getAmount();
+                        case DEPOSIT:
+                            return transaction.getAmount();
+                        default:
+                            return 0;
+                    }
+                }).sum();
+    }
+
+    public void deductTaxes() {
+        for (Account account : accounts) {
+            if (Taxable.class.isAssignableFrom(account.getClass())) {
+                Taxable taxable = (Taxable) account;
+                taxable.tax(getIncome(taxable));
+            }
+        }
     }
 
 }
